1563d7bdcf0178f8c2a7fe287e5135007a69c745
ErandaRandikaRox <160154662+ErandaRandikaRox@users.noreply.github.com>
2025-03-19 12:45:51 +0530

    firebase configure for both android and iOS

--- food_delivery_app/lib/main.dart
+++ food_delivery_app/lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/auth/loging_or_register.dart'; // Corrected typo in path
 import 'package:food_delivery_app/models/resturant.dart'; // Corrected typo in 'Restaurant'
 import 'package:food_delivery_app/themes/theme_provider.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await Firebase.initializeApp(); // Ensure Firebase initializes correctly
+  } catch (e) {
+    print('Firebase initialization failed: $e');
+  }
   runApp(
     MultiProvider(
       providers: [
